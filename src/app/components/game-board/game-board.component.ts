15ea761beaeb325a68eece6e23953c581608d6de
Yevhenii Danoi <geneforapp@gmail.com>
2023-11-01 12:20:08 +0200

    determine the method to determine the winner

--- src/app/components/game-board/game-board.component.ts
+++ src/app/components/game-board/game-board.component.ts
@@ -16,7 +16,65 @@ export class GameBoardComponent {
   onCellClick(row: number, col: number): void {
     if (this.gameBoard[row][col] === '') {
       this.gameBoard[row][col] = this.currentPlayer;
+
+      const winner: string | null = this.checkForWinner();
+
+      /**TODO: Determine the winnerd count in the state when we define the user components */
+      if (winner) {
+        console.log(`Player ${winner} is won.`);
+
+        this.resetGame();
+      }
+
       this.currentPlayer = this.currentPlayer === 'X' ? 'O' : 'X';
     }
+  }
+
+  private checkForWinner(): string | null {
+    //Check horizontal rows
+    for (let row of this.gameBoard) {
+      if (row[0] === row[1] && row[0] === row[2] && row[0] !== '') {
+        return row[0];
+      }
+    }
+
+    //Check vertical columns
+    for (let col = 0; col < 3; col++) {
+      if (
+        this.gameBoard[0][col] === this.gameBoard[1][col] &&
+        this.gameBoard[0][col] === this.gameBoard[2][col] &&
+        this.gameBoard[0][col] !== ''
+      ) {
+        return this.gameBoard[0][col];
+      }
+    }
+
+    //Check diagonals
+    if (
+      this.gameBoard[0][0] === this.gameBoard[1][1] &&
+      this.gameBoard[0][0] === this.gameBoard[2][2] &&
+      this.gameBoard[0][0] !== ''
+    ) {
+      return this.gameBoard[0][0];
+    }
+
+    if (
+      this.gameBoard[0][2] === this.gameBoard[1][1] &&
+      this.gameBoard[0][2] === this.gameBoard[2][0] &&
+      this.gameBoard[0][2] !== ''
+    ) {
+      return this.gameBoard[0][2];
+    }
+
+    return null;
+  }
+
+  resetGame(): void {
+    this.gameBoard = [
+      ['', '', ''],
+      ['', '', ''],
+      ['', '', ''],
+    ];
+    this.currentPlayer = 'X';
   }
 }
