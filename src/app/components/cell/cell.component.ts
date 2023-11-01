414e70a12aa0cdacf17a33cc47ad261f191208d7
Yevhenii Danoi <geneforapp@gmail.com>
2023-11-01 11:45:17 +0200

    create components for the playing field and cell

---
+++ src/app/components/cell/cell.component.ts
@@ -0,0 +1,10 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-cell',
+  templateUrl: './cell.component.html',
+  styleUrls: ['./cell.component.css'],
+})
+export class CellComponent {
+  @Input() value?: string;
+}
