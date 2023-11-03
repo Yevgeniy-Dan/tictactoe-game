6ee35f1e026cf49a3d0192d8e592bc2b805b6a1d
Yevhenii Danoi <geneforapp@gmail.com>
2023-11-03 17:08:24 +0200

    define static game-score component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,26 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { GameBoardComponent } from './components/game-board/game-board.component';
 
 import { MatGridListModule } from '@angular/material/grid-list';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatCardModule } from '@angular/material/card';
+import { MatDividerModule } from '@angular/material/divider';
+
+import { GameScoreComponent } from './components/game-score/game-score.component';
 
 @NgModule({
-  declarations: [AppComponent, GameBoardComponent],
+  declarations: [AppComponent, GameBoardComponent, GameScoreComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
     MatGridListModule,
     BrowserAnimationsModule,
+    MatCardModule,
+    MatDividerModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
